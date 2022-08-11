eb2c9a9e4cefbe886662bdb3026a6e79f7067d50
Jovan Ceperkovic <jovanceperkovic96@gmail.com>
2022-08-11 15:31:41 +0200

    Config test 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { Button } from '../components/Button';
 
 const Home: NextPage = () => (
   <div>
+    {/* eslint-disable-next-line no-nested-ternary */}
+    {false ? 1 : true ? 2 : 3}
     <p>Hello world test</p>
     <Button count={2} />
   </div>
